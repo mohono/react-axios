b126e51bf511fe1a8c66df3a0206807b3b50741c
mohono <mohono209@gmail.com>
2024-05-29 10:42:23 +0330

    edit environment variables

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {process.env.secret}
+          {process.env.REACT_APP_SECRET}
         </a>
       </header>
     </div>
